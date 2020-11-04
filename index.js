8a44ade19ca7badd00c8c1a846577a1152c06133
inokawa <48897392+inokawa@users.noreply.github.com>
2020-11-04 15:18:31 +0900

    Check os with Platform.OS

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-import { NativeModules, NativeEventEmitter } from "react-native";
+import { NativeModules, NativeEventEmitter, Platform } from "react-native";
 
 const { Wasm } = NativeModules;
 const eventEmitter = new NativeEventEmitter(Wasm);
 
-const instantiate = (bytes) =>
-  new Promise((resolve, reject) => {
-    const subscription = eventEmitter.addListener(
-      "wasmResolved",
-      (methodNames) => {
-        subscription.remove();
-        resolve({
-          instance: {
-            exports: JSON.parse(methodNames).reduce((acc, k) => {
-              acc[k] = (...args) => Wasm.call(k, JSON.stringify(args));
-              return acc;
-            }, {}),
-          },
-          module: {},
-        });
-      }
-    );
-
-    Wasm.instantiate(bytes.toString())
-      .then((res) => {
-        if (!res) {
+if (Platform.OS === "ios") {
+  const instantiate = (bytes) =>
+    new Promise((resolve, reject) => {
+      const subscription = eventEmitter.addListener(
+        "wasmResolved",
+        (methodNames) => {
           subscription.remove();
-          reject("failed to contact to webview");
+          resolve({
+            instance: {
+              exports: JSON.parse(methodNames).reduce((acc, k) => {
+                acc[k] = (...args) => Wasm.call(k, JSON.stringify(args));
+                return acc;
+              }, {}),
+            },
+            module: {},
+          });
         }
-      })
-      .catch((e) => {
-        subscription.remove();
-        reject(e);
-      });
-  });
+      );
 
-window.WebAssembly = {
-  instantiate: (bytes, importObject) => instantiate(bytes),
-  instantiateStreaming: (response, importObject) =>
-    Promise.resolve(response.arrayBuffer()).then((bytes) => instantiate(bytes)),
-  compile: (bytes) => {},
-  compileStreaming: () => {},
-  validate: () => true,
-};
+      Wasm.instantiate(bytes.toString())
+        .then((res) => {
+          if (!res) {
+            subscription.remove();
+            reject("failed to contact to webview");
+          }
+        })
+        .catch((e) => {
+          subscription.remove();
+          reject(e);
+        });
+    });
+
+  window.WebAssembly = {
+    instantiate: (bytes, importObject) => instantiate(bytes),
+    instantiateStreaming: (response, importObject) =>
+      Promise.resolve(response.arrayBuffer()).then((bytes) =>
+        instantiate(bytes)
+      ),
+    compile: (bytes) => {},
+    compileStreaming: () => {},
+    validate: () => true,
+  };
+}
