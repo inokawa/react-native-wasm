e64b51daf3413024607fc262a775f09781683324
inokawa <48897392+inokawa@users.noreply.github.com>
2021-01-04 02:17:09 +0900

    Implement callSync for Android

--- android/src/main/java/com/reactnativewasm/WasmModule.kt
+++ android/src/main/java/com/reactnativewasm/WasmModule.kt
@@ -9,6 +9,7 @@ import android.webkit.WebView
 import androidx.annotation.RequiresApi
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
+import java.util.concurrent.CountDownLatch
 
 
 const val js: String = """
@@ -71,23 +72,45 @@ class WasmModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaMo
             override fun run() {
                 webView.evaluateJavascript("""
                     javascript:instantiate("$id", [$bytes]);
-                    """, ValueCallback<String> {
-                    // TODO handle error
-                    reply ->
-                    promise.resolve(true)
+                    """, ValueCallback<String> { value ->
+                    {
+                        // TODO handle error
+                        if (value == null) {
+                            promise.reject("error")
+                        } else {
+                            promise.resolve(true)
+                        }
+                    }
                 })
             }
         });
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
-    fun callSync(id: String, name: String, args: String, promise: Promise) {
-        this.webView.loadUrl("""
-            javascript:wasm["$id"].instance.exports.$name(...$args);
-            """)
-        // TODO return value synchronously
-        // TODO handle error
-        promise.resolve(true)
+    fun callSync(id: String, name: String, args: String): Int {
+        var result: Int = 0
+        val latch = CountDownLatch(1)
+        Handler(Looper.getMainLooper()).post(object : Runnable {
+            @RequiresApi(Build.VERSION_CODES.KITKAT)
+            override fun run() {
+                webView.evaluateJavascript("""
+                    javascript:wasm["$id"].instance.exports.$name(...$args);
+                    """, ValueCallback<String> { value ->
+                    {
+                        // TODO handle error
+                        if (value == null) {
+                            result = 0
+                        } else {
+                            result = value as Int
+                        }
+                        latch.countDown();
+                    }
+                })
+            }
+        });
+
+        latch.await()
+        return result
     }
 
     fun sendEvent(eventName: String, params: String?) {
