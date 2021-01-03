f79d55eb0c63a24dbdd1f54ce3b6497827b827d0
inokawa <48897392+inokawa@users.noreply.github.com>
2021-01-03 21:08:50 +0900

    Add Platform.OS === "android" to condition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 import { WebAssembly as WasmPolyfill } from "./WebAssembly";
 
-if (Platform.OS === "ios") {
+if (Platform.OS === "ios" || Platform.OS === "android") {
   window.WebAssembly = window.WebAssembly || WasmPolyfill;
 }
