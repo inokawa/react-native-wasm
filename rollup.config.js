2902729af358e16f893f311ec80425e9847be51a
inokawa <48897392+inokawa@users.noreply.github.com>
2021-12-18 16:27:02 +0900

    Remove bob

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+    ],
+    plugins: [typescript()],
+  },
+];
